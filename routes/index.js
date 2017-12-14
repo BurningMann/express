f0432f5c88445cafa81db8d87ff7df2c46868ca8
группы стационара <st@itstep.lan>
2017-12-14 21:15:42 +0300

    gg

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,19 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/:id?', function(req, res, next) {
+	if(req.params.id){
+		var index = req.params.id;
+	}
+	else{
+		var index = 'welcome';
+	}
+	console.log(index);
+  res.render('index', { title: index });
 });
-
+router.post('/reg', function(req,res,next){
+	console.log(req.body.name);
+	console.log(req.body.pass);
+	res.redirect('/');
+});
 module.exports = router;
